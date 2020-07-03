592e47633a37452812e642fc70be57385304d2dc
Balazs Ferencz <ferenczbalazs85@gmail.com>
2020-07-03 10:02:53 +0100

    kozep
    kozep

--- submit.js
+++ submit.js
@@ -11,7 +11,7 @@ function createDiv(count) {
    let today = new Date().toLocaleDateString();   
    var div = document.createElement("div");
    div.style.width = "1vp";
-   div.style.height = "2rem";
+   div.style.height = "20px";
    div.style.textAlign = "center";
    div.style.color = "white";
    div.style.fontFamily = "'Orbitron', sans-serif "; 
@@ -22,8 +22,64 @@ function createDiv(count) {
 
 document.getElementById("wrp").appendChild(div);
 
-console.log(today, count);
+// console.log(today, count);
 
 
+let data = { 
+      "id": 0,
+      "date": today,
+      "time": count 
+   };
 
+
+fetch( 'http://localhost:3000/entries', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Success:', data);
+})
+.catch((error) => {
+  console.error('Error:', error);
+});
+
+
+}  
+
+function getData() {
+   fetch('http://localhost:3000/entries')
+  .then(response => response.json())
+  .then(data => createRsp(data));
+}
+
+
+function createRsp(data) {
+  let lines = {};
+  for(let i = 0; i < data.length; i++) {
+     date = data[i].date;
+     time = data[i].time;
+  }
+
+  console.log(data);
+  console.log(date, time);
+ 
+  createLine(date, time);
+}
+
+function createLine(date, time) {
+      let line = document.createElement("line");
+      line.style.width = "100%";
+      line.style.fontSize = "20px";
+      line.style.textAlign = "center";
+      line.style.color = "white";
+      line.style.fontFamily = "'Orbitron', sans-serif "; 
+      line.style.padding = "5px";
+      line.style.paddingTop = "5px";
+      line.innerHTML = "On " + date + " you have studied " + time + " s!!";
+
+document.getElementById("dataDiv").appendChild(line);
 }  
